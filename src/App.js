1748cee314753c419d7ca4159981c1945a76c6d2
kenzo7664 <olaotanemmanuel1@gmail.com>
2021-10-12 15:11:44 +0100

    corrections made

--- src/App.js
+++ src/App.js
@@ -7,23 +7,31 @@ function App() {
   const [isLoading, setIsLoading] = useState(true)
   const [term,setTerm] = useState('')
 
-  useEffect(()=>{
+  const fetchData =()=>{
     fetch(`https://eze-mobile-api-staging.herokuapp.com/api/v1/products/price?category=Smartphones&brand=Apple&sort=lowestAsk&hoursInterval=24&limit=20&page=1&slugId=`)
     .then(res => res.json())
-    .then(data => {
-      setImages(data.data)
+    .then(json => {
+      console.log(json.data.data);
+      const images = json.data.data
+      setImages(images)
       setIsLoading(false)
     })
+   
     .catch(err => console.log(err))
+  
+  } 
+  useEffect(()=>{
+     fetchData()
   },[])
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-3 gap-4">
-      {images.map(image => (
-          <PhoneCard key ={image.id} image={image} />
-        ))
-        }
+       {images.map(image =>(
+         <PhoneCard key ={image._id} image={image}/>
+       ))}
       </div>
+      
+      
     </div>
 
   );
--- src/components/PhoneCard.js
+++ src/components/PhoneCard.js
@@ -1,19 +1,20 @@
 import React from 'react'
 
-const PhoneCard = () => {
+const PhoneCard = ({ image }) => {
+
     return (
         <div className="max-w-sm rounded overflow-hidden bg-purple-900 shadow-lg">
         <span className="border border-black float-right m-2 p-1">B1</span>
-        <img src="https://source.unsplash.com/random" alt="" className ="h-15 px-6 py-4" />
+        <img src={image.imgUrl} alt="" className ="h-15 px-6 py-4" />
         <div className="px-6 py-4">
           <div className="font-bold text-white text-xl mb-1">
-            IPhone 7
+            {image.name}
           </div>
           <div className="font-semibold text-white"> Unlocked | 256Gb</div>
           <span className="font-normal text-white">Unit price</span>
-          <h1 className="font-bold text-white">$450</h1>
+          <h1 className="font-bold text-white">$459{image.rank}</h1>
           <span className="font-normal text-white">1500 available</span>
-         
+           
         </div>
         <div className="flex flex-col justify-center items-center mb-4">
          <button className="text-white border border-white p-2 w-20 rounded bg-blue-500 ">BUY</button>
